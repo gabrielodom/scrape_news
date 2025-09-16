72f146fe2f57487aff332b4ad391d5afd40cc050
gabrielodom <gabriel.odom@fiu.edu>
2025-09-16 15:28:09 -0400

    adding basic GitHub Actions setup

---
+++ R/scrape_news.r
@@ -0,0 +1,18 @@
+# Script to scrape news
+# Gabriel Odom
+# 2025-09-16
+
+
+library(httr2)
+
+date <- Sys.Date() - 1
+req <- request("https:///newsapi.org/v2/everything") %>%
+  req_url_query(
+    # keyword here:
+    q = '`"data science"`',
+    from = date,
+    pageSize = 10,
+    # notes for how to set this are in the diamonds/ repo
+    apiKey = Sys.getenv("NEWS_API_KEY")
+  )
+req_perform(req, path = paste0("data_news/", date, ".json"))
---
+++ R/workshop_setup_20250916.r
@@ -0,0 +1,12 @@
+# practice scraping news results
+# Gabriel Odom
+# 2025-09-16
+
+
+# We have code here that we want to run:
+source("R/scrape_news.r")
+
+# We want this to run via a GitHub Action, so we need to set this up:
+usethis::use_github_action()
+# These options aren't helpful. I asked ChatGPT to write me one. Now that we
+# have this, I'm going to commit and push to GitHub to see what happens.
