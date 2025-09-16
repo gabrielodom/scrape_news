5883a7f3526c6004c3067d97b044713c10747f52
gabrielodom <gabriel.odom@fiu.edu>
2025-09-16 15:30:18 -0400

    maybe my name: field in the .yaml was wrong?

--- R/workshop_setup_20250916.r
+++ R/workshop_setup_20250916.r
@@ -10,3 +10,5 @@ source("R/scrape_news.r")
 usethis::use_github_action()
 # These options aren't helpful. I asked ChatGPT to write me one. Now that we
 # have this, I'm going to commit and push to GitHub to see what happens.
+
+# I committed my changes and pushed, but nothing happened in the Actions tab.
