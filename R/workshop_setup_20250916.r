d41d16ac6f0aff010b9c91f936fdca64f28ccac0
gabrielodom <gabriel.odom@fiu.edu>
2025-09-16 15:46:22 -0400

    now on to the next error...

--- R/workshop_setup_20250916.r
+++ R/workshop_setup_20250916.r
@@ -14,3 +14,23 @@ usethis::use_github_action()
 # I committed my changes and pushed, but nothing happened in the Actions tab.
 # Ah, maybe because the .yaml file was looking at the "master" branch instead of
 # the "main" branch?
+# YAY! The GitHub Action is actually working. Not sure if it will run without
+# errors, but something is happening.
+
+# First error:
+# Run Rscript scrape_news.R
+# Fatal error: cannot open file 'scrape_news.R': No such file or directory
+# Error: Process completed with exit code 2.
+# I think this is because the script is in R/. Let's fix that.
+
+# Second error:
+
+# So we need to make sure to point to the API key in a place that GitHub can
+# find it. First, we need to set the API key in GitHub; so we go to settings for
+# the repository, then scroll down to Secrets and Variables, click on Actions,
+# then add a repository secret. Create a secret named NEWS_API_KEY then store
+# the value in the matching field. Then, we also need the configuration file to
+# tell GitHub where to find that key. So, in the section for "Run your script",
+# add an environment variable with:
+# env:
+#   NEWS_API_KEY: ${{ secrets.NEWS_API_KEY }}
